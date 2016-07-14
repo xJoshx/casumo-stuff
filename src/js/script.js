27a61d38f40645913ace1677d3c553eb5caa9370
José Luis Vizcaíno Martínez <jlvizmar@gmail.com>
2016-07-14 20:19:56 +0200

    Add some listeners and text to sidebar

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,32 @@
 function casumoActions () {
-  this.sidebarIsToggled = false;
+
 };
 
 casumoActions.prototype.toggleSidebar = function (event) {
   var sidebar = document.getElementById('sidebar'),
       close = document.getElementById('close-sidebar'),
-      blur = document.getElementById('sidebar-blur');
+      blur = document.getElementById('sidebar-blur'),
+      sidebarVisibleClasses = 'sidebar visible';
 
-  if (this.sidebarIsToggled) {
+  if (sidebar.className === sidebarVisibleClasses) {
     sidebar.className = 'sidebar closed'
     close.className = 'header__close-sidebar'
     blur.className = 'sidebar-blur closed'
   } else {
     sidebar.className = 'sidebar visible'
-    close.className += ' visible'
+    close.className = 'header__close-sidebar fi-close visible'
     blur.className = 'sidebar-blur visible'
   }
 
   this.sidebarIsToggled = !this.sidebarIsToggled
 }
 
+casumoActions.prototype.listenToClicksBlur = function (toggleFunction) {
+  var blur = document.getElementById('sidebar-blur')
+
+  blur.addEventListener('click', toggleFunction)
+}
+
 var casumoActions = new casumoActions()
+
+casumoActions.listenToClicksBlur(casumoActions.toggleSidebar)
