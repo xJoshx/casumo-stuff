3108a063ac17202a6e17b8d0b072b390ec22a09c
José Luis Vizcaíno Martínez <jlvizmar@gmail.com>
2016-07-14 09:59:32 +0200

    A lot of improvements

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,20 @@ function casumoActions () {
 };
 
 casumoActions.prototype.toggleSidebar = function (event) {
-  var sidebar = document.getElementById('sidebar');
+  var sidebar = document.getElementById('sidebar'),
+      close = document.getElementById('close-sidebar'),
+      blur = document.getElementById('sidebar-blur');
 
   if (this.sidebarIsToggled) {
-    sidebar.className = 'sidebar'
+    sidebar.className = 'sidebar closed'
+    close.className = 'header__close-sidebar'
+    blur.className = 'sidebar-blur closed'
   } else {
-    sidebar.className += ' visible'
+    sidebar.className = 'sidebar visible'
+    close.className += ' visible'
+    blur.className = 'sidebar-blur visible'
   }
-  
+
   this.sidebarIsToggled = !this.sidebarIsToggled
 }
 
