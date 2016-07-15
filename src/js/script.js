6b2be7d57850fc9968981f906e7b0156b5b61101
José Luis Vizcaíno Martínez <jlvizmar@gmail.com>
2016-07-15 09:06:16 +0200

    Minor fixes

--- src/js/script.js
+++ src/js/script.js
@@ -6,16 +6,19 @@ casumoActions.prototype.toggleSidebar = function (event) {
   var sidebar = document.getElementById('sidebar'),
       close = document.getElementById('close-sidebar'),
       blur = document.getElementById('sidebar-blur'),
+      html = document.getElementsByTagName('html')[0],
       sidebarVisibleClasses = 'sidebar visible';
 
   if (sidebar.className === sidebarVisibleClasses) {
     sidebar.className = 'sidebar closed'
     close.className = 'header__close-sidebar'
     blur.className = 'sidebar-blur closed'
+    html.style.overflowY = 'scroll'
   } else {
     sidebar.className = 'sidebar visible'
     close.className = 'header__close-sidebar fi-close visible'
     blur.className = 'sidebar-blur visible'
+    html.style.overflowY = 'hidden'
   }
 
   this.sidebarIsToggled = !this.sidebarIsToggled
@@ -34,7 +37,7 @@ casumoActions.prototype.scrollEffect = function (event) {
     sidebarItems = document.getElementById('sidebar-scroll-effect'),
     sidebar = document.getElementById('sidebar'),
     sidebarHeight = 900
-    screenWidth = 1126
+    screenWidth = 768
 
     if(window.screen.width >= screenWidth) {
       if (top < 590) {
